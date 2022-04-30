6177786e6deb3e846b67b4999ec020b5962bc5b5
Ricky Chuang <songsongyyohya@gmail.com>
2022-04-30 23:04:20 +0800

    Initial Commit

---
+++ Candy-Crush-iOS-Game/Candy_Crush_iOS_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Candy_Crush_iOS_GameApp.swift
+//  Candy-Crush-iOS-Game
+//
+//  Created by 莊智凱 on 2022/4/30.
+//
+
+import SwiftUI
+
+@main
+struct Candy_Crush_iOS_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Candy-Crush-iOS-Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Candy-Crush-iOS-Game
+//
+//  Created by 莊智凱 on 2022/4/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
