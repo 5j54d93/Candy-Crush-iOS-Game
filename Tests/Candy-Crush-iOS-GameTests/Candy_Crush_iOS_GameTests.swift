33f88b586656d7b48e4257d1d937240cd3a09b31
Ricky Chuang <songsongyyohya@gmail.com>
2022-05-08 17:22:39 +0800

    Create Candy_Crush_iOS_GameTests.swift

---
+++ Tests/Candy-Crush-iOS-GameTests/Candy_Crush_iOS_GameTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import Candy_Crush_iOS_Game
+
+final class Candy_Crush_iOS_GameTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(Candy_Crush_iOS_Game().text, "Hello, World!")
+    }
+}
